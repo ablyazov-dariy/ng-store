eb346f4c42c73d58d6a5640c5a50b645176b8549
Ablyazov Dariy <mr96crusher@gmail.com>
2023-06-30 11:47:59 +0200

    add dialog with maps to contacts page

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.css'],
 })
-export class ContactsComponent {
+export class ContactsComponent implements AfterViewInit {
+  @ViewChild('mapsModal') mapsModal?: ElementRef<HTMLDialogElement>;
 
+  ngAfterViewInit(): void {
+    console.log(this.mapsModal);
+  }
+
+  onModalClick(event: MouseEvent): void {
+    const dialogDimensions: DOMRect = this.mapsModal!.nativeElement.getBoundingClientRect();
+    console.log(event);
+    if (
+      event.clientX < dialogDimensions.left ||
+      event.clientX > dialogDimensions.right ||
+      event.clientY < dialogDimensions.top ||
+      event.clientY > dialogDimensions.bottom
+    ) {
+      this.mapsModal!.nativeElement.close();
+    }
+  }
 }
