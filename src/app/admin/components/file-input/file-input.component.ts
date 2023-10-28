7ef88f45ffec9a55355dd732423bb2d0549a8dde
Ablyazov Dariy <mr96crusher@gmail.com>
2023-10-29 00:03:22 +0200

    fix file errors

--- src/app/admin/components/file-input/file-input.component.ts
+++ src/app/admin/components/file-input/file-input.component.ts
@@ -1,22 +1,63 @@
-import { Component, Input } from '@angular/core';
-import { ControlValueAccessor, FormControl } from '@angular/forms';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-file-input',
   templateUrl: './file-input.component.html',
   styleUrls: ['./file-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: FileInputComponent,
+      multi: true,
+    },
+  ],
 })
-export class FileInputComponent implements ControlValueAccessor {
-  @Input({ required: true, alias: 'formControl' }) control?: FormControl;
+export class FileInputComponent implements ControlValueAccessor, OnInit {
+  public disabled: boolean = false;
+  private onChange?: (value: File | null) => void;
+  private onTouched?: () => void;
 
-  onFileUpload(event: Event): void {
-    // todo: img upload
-    this.control?.patchValue(URL.createObjectURL((event.target as HTMLInputElement).files![0]));
+  file = signal<File | string | null>(null);
+
+  url = computed((): void | string => {
+    const file = this.file();
+    if (!file) return;
+    if (typeof file !== 'string') {
+      try {
+        return URL.createObjectURL(file);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      return file as string;
+    }
+  });
+
+  ngOnInit(): void {}
+
+  writeValue(obj: File): void {
+    this.file.set(obj);
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 
-  registerOnChange(fn: any): void {}
+  onFileChange(input: HTMLInputElement) {
+    const file = input.files ? input.files[0] : null;
+    if (file) {
+      this.file.set(file);
+      this.onChange?.(file);
+    }
+  }
 
-  registerOnTouched(fn: any): void {}
-
-  writeValue(obj: any): void {}
+  onInputTouched() {
+    this.onTouched?.();
+  }
 }
