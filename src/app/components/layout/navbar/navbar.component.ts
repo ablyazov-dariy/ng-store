3f14b0c9e5bad50f2199f90be0b474a41c29913a
Ablyazov Dariy <mr96crusher@gmail.com>
2023-12-06 13:50:55 +0100

    fix min height

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  @Input({ required: true }) showMenu = false;
+  @Output() menuClicks: EventEmitter<void> = new EventEmitter();
+}
