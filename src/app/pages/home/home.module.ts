7d53b0f442518c1548505b0bc409cd350b735f8b
Ablyazov Dariy <mr96crusher@gmail.com>
2023-12-11 17:48:33 +0100

    change login to form

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatRadioModule } from '@angular/material/radio';
@@ -8,12 +8,12 @@ import { MatRadioModule } from '@angular/material/radio';
 import { SharedModule } from '@shared/shared.module';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CollectionPreviewComponent } from './collection-preview/collection-preview.component';
+import { HeroCardComponent } from './hero-card/hero-card.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { HeroMainComponent } from './hero-main/hero-main.component';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { HeroMainComponent } from './hero-main/hero-main.component';
-import { HeroCardComponent } from './hero-card/hero-card.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +33,7 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
     NgOptimizedImage,
     MatRadioModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
 })
